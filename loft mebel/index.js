2fe17938a92525dcf0cc599442fda46d1200b77e
Shahzoda <165678973+Shahz0daxon@users.noreply.github.com>
2024-05-23 11:34:27 +0500

    loft_mebel

---
+++ loft mebel/index.js
@@ -0,0 +1,140 @@
+const mebellar = [
+  {
+    id: "1",
+    url: "https://cdn0.divan.by/img/v1/VU6xRGz4h8-3X6Rkt2w7Kpc3D5mkdhaExfK3Ef0OVI4/t:0::0:0/pd:123:60:123:60/rs:fit:1148:594:0:1:ce:0:0/g:ce:0:0/bg:f5f3f1/q:85/czM6Ly9kaXZhbi9wcm9kdWN0LzQ4MzcyMTIucG5n.jpg",
+    title: 'Толикс-2 White Gloss',
+    text: 'Барные стулья',
+    text2: '4 690₽',
+
+  },
+  {
+    id: "2",
+    url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRxhTSOF6Dt9BHgSMl-GXm4WPRwJg59kH97CXjpJgtW4w&s",
+    title: 'Динс Velvet Yellow',
+    text: 'Диваны',
+    text2: '28 490₽',
+
+  },
+  {
+    id: "3",
+    url: "https://cdn0.divan.by/img/v1/GY46rj541rhDFJPrOMZldIxQV2VdvoRle0qfk0u6vzk/t:0::0:0/pd:60:60:60:60/rs:fit:1148:720:0:1:ce:0:0/g:ce:0:0/bg:f5f3f1/q:85/czM6Ly9kaXZhbi9wcm9kdWN0LzUwMTM1ODIucG5n.jpg",
+    title: 'Кускен Navy Blue',
+    text: 'Диваны',
+    text2: '2 300₽',
+
+  },
+  {
+    id: "4",
+    url: "https://vmp59.com/uploadedFiles/eshopimages/icons/285x285/proyva-1.jpg",
+    title: 'Динс Velvet Yellow',
+    text: 'Диваны',
+    text2: '28 490₽',
+  },
+  {
+    id: "5",
+    url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTUfXbDCsVPQbGwmENoUQSz_z4jxMYkyrZpgx5eLS8Dwg&s",
+    title: 'Толикс-2 White Gloss',
+    text: 'Диваны',
+    text2: '28 490₽',
+  },
+  {
+    id: "6",
+    url: "https://images.tomas.kz/i3/firms/111/5337/5337286/komod-tiffani-4s-vudlayn-kremovyy-belarus-anreks_19712ac68428523_500x500_1.webp.jpg",
+    title: 'Кускен Navy Blue',
+    text: 'Диваны',
+    text2: '28 490₽',
+  },
+
+  {
+    id: "7",
+    url: "https://cdn0.divan.by/img/v1/JtWM87xvcT7sOSDpH6L1ASqmi1vr52h7NgqnWa8rl40/t:0::0:0/pd:60:60:60:60/rs:fit:1148:720:0:1:ce:0:0/g:ce:0:0/bg:f5f3f1/q:95/czM6Ly9kaXZhbi9wcm9kdWN0LzQxNTgzNTIucG5n.jpg",
+    title: 'Шерона Barhat Grey',
+    text: 'Диваны',
+    text2: '28 490₽',
+  },
+  {
+    id: "7",
+    url: "https://pngimg.com/d/table_PNG7006.png",
+    title: 'Сан-Паулу Velvet Brown',
+    text: 'Диваны',
+    text2: '28 490₽',
+  },
+  {
+    id: "8",
+    url: "https://i.pinimg.com/736x/fb/55/76/fb5576d6320f3254b20b7cc4b52ece9b.jpg",
+    title: 'Шерона Barhat Grey',
+    text: 'Диваны',
+    text2: '28 490₽',
+  },
+  {
+    id: "9",
+    url: "https://images.uzum.uz/cllbm55enntcj8a9t8og/original.jpg",
+    title: 'Шерона Barhat Grey',
+    text: 'Диваны',
+    text2: '28 490₽',
+  },
+  {
+    id: "10",
+    url: "https://images.uzum.uz/ckl4v84vutv26iiai3mg/original.jpg",
+    title: 'Шерона Barhat Grey',
+    text: 'Диваны',
+    text2: '28 490₽',
+  },
+  {
+    id: "11",
+    url: "https://images.uzum.uz/cl92a4fn7c6qm23ip44g/original.jpg",
+    title: 'Шерона Barhat Grey',
+    text: 'Диваны',
+    text2: '28 490₽',
+  },
+  {
+    id: "12",
+    url: "https://images.uzum.uz/ckojf03k9fq6u2tdr2eg/original.jpg",
+    title: 'Шерона Barhat Grey',
+    text: 'Диваны',
+    text2: '28 490₽',
+  },
+  {
+    id: "13",
+    url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRNbicuTqx3HBuxDmXt__raPIFmkTNGa0XscmdML7h1kk2IcIhI7g9CmTsxq_YzRUXO5to&usqp=CAU",
+    title: 'Шерона Barhat Grey',
+    text: 'Диваны',
+    text2: '28 490₽',
+  },
+  {
+    id: "11",
+    url: "https://cdn0.divan.ru/img/v1/A1gnRyBE4aINbtyyh0jIsV8oWnOP8OO8LVnjw3JoKkI/t:0::0:0/pd:60:60:60:60/rs:fit:1148:720:0:1:ce:0:0/g:ce:0:0/bg:f5f3f1/q:95/czM6Ly9kaXZhbi9wcm9kdWN0LzQxNTIxMzcucG5n.jpg",
+    title: 'Шерона Barhat Grey',
+    text: 'Диваны',
+    text2: '28 490₽',
+  },
+  {
+    id: "11",
+    url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQas1k5qBsN5-sTfI3-6ZkmHo-WXnQfRAL77oF5kqq4dA&s",
+    title: 'Тиффани Вудлайн Крем',
+    text: 'Диваны',
+    text2: '28 490₽',
+  },
+
+
+];
+
+let row = document.querySelector('.row')
+
+let html = '';
+
+
+mebellar.forEach((item) => {
+  html += `  <div class="card">
+     <img src="${item.url}" class="card-img-top" alt="..." />
+     <div class="card-body">
+       <h5 class="card-title">${item.title}</h5>
+       <p class="card-text">
+         ${item.text}
+       </p>
+       <p>${item.text2}</p>
+       <button class="btn">Go somewhere></button>
+     </div>
+   </div>`
+});
+row.innerHTML = html
